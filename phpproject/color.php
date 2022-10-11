98719460dac9fca3d6f568f15fad580ae2cc427b
Ramkumar120 <viratrambe051999@gmail.com>
2022-10-11 11:04:49 +0530

    first commit

---
+++ phpproject/color.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+    <?php
+$colors=['Red','Blue','Green','Yellow','White','Black'];
+foreach ($colors as $values) {
+	echo ("<ul><li> Color:$values</li></ul>");
+}
+?>
+    </body>
+</html>
